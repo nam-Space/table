5f9883b52b67b4b2ad8238507deab54c3c798566
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-10-13 12:11:17 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const students = [
+    {
+      company: 'Alfreds Futterkiste',
+      contact: 'Maria Anders',
+      country: 'Germany'
+    },
+    {
+      company: 'Centro comercial Moctezuma',
+      contact: 'Francisco Chang',
+      country: 'Mexico'
+    },
+    {
+      company: 'Ernst Handel',
+      contact: 'Roland Mendel',
+      country: 'Austria'
+    },
+    {
+      company: 'Island Trading',
+      contact: 'Helen Bennett',
+      country: 'UK'
+    },
+    {
+      company: 'Laughing Bacchus Winecellars',
+      contact: 'Yoshi Tannamuri',
+      country: 'Canada'
+    },
+    {
+      company: 'Magazzini Alimentari Riuniti',
+      contact: 'Giovanni Rovelli',
+      country: 'Italy'
+    }
+]
+
+const table = students.map((student, index) => (
+    <tr key={index}>
+        <td>{student.company}</td>
+        <td>{student.contact}</td>
+        <td>{student.country}</td>
+    </tr>
+))
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(table)
